72f7917edd6649685f5db157d7faf9ea7f0af2ee
Harsha-8899 <harshachakravarthy88@gmail.com>
2024-07-23 18:55:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,35 +27,6 @@ def load_original_data():
         st.error("Failed to load data from GitHub.")
         return None
 
-# Creating a function for prediction
-def prediction(input_data):
-    # Changing the input_data to a numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-    # Reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    # Make prediction
-    prediction = loaded_model.predict(input_data_reshaped)
-    # Display the prediction
-    if prediction[0] == 0:
-        return 'The predicted Vehicle type is Battery Electric Vehicle'
-    else:
-        return 'The predicted Vehicle type is Plug-in Hybrid Electric Vehicle'
 
-def main():
-    # Giving a title
-    st.title('EV Prediction Web App')
-    Postal_code = st.text_input("Postal Code")
-    Model_year = st.text_input("Model Year")
-    Make = st.text_input("Make")
-    Model = st.text_input("Model")
-    Electric_range = st.text_input("Electric Range")
-    Base_MSRP = st.text_input("Base MSRP")
-    # Code for Prediction
-    EV_type = ''
-    # Creating a button for Prediction
-    if st.button('Electric Vehicle Type:'):
-        EV_type = prediction([Postal_code, Model_year, Make, Model, Electric_range, Base_MSRP])
-    st.success(EV_type)
-
-if __name__ == '__main__':
-    main()
+st.write("### Dataset")
+st.write(data.head())
