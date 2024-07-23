55d0e31f6433fcbb48ea9b43e98acb3747b45830
Harsha-8899 <harshachakravarthy88@gmail.com>
2024-07-23 19:03:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix, classification_report, roc_auc_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
 # Title and description
 st.title("Electric Vehicle Data Analysis")
 st.write("An interactive web app to analyze and visualize electric vehicle data.")
