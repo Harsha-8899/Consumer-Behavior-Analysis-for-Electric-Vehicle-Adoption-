03605cb39b38e9f4f2681e9a21b4e8cf4b1d02dd
Harsha-8899 <harshachakravarthy88@gmail.com>
2024-07-23 19:19:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ import numpy as np
 st.title("Electric Vehicle Data Analysis")
 st.write("An interactive web app to analyze and visualize electric vehicle data.")
 
-# Load the data
-data = pd.read_csv('https://github.com/Harsha-8899/Consumer-Behavior-Analysis-for-Electric-Vehicle-Adoption-/blob/main/Electric_Vehicle_Population_Data.xlsx')
+url = 'https://github.com/Harsha-8899/Consumer-Behavior-Analysis-for-Electric-Vehicle-Adoption-/blob/main/Electric_Vehicle_Population_Data.xlsx'
+df = pd.read_xlsx(url,index_col=0,parse_dates=[0])
 
-# Display the data
-st.write(data.head())
+print df.head(5)
