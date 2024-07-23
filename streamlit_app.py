50c02dc67a583ae9e85b46f43fe87ad805af6f4f
Harsha-8899 <harshachakravarthy88@gmail.com>
2024-07-23 18:56:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,6 @@ import streamlit as st
 st.subheader('I am Harsha!')
 st.text('My First Streamlit Project')
 
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  6 17:42:13 2024
-
-@author: supre
-"""
-
-
 import numpy as np
 import pickle
 import streamlit as st  # Added import statement for Streamlit
@@ -27,4 +18,5 @@ def load_original_data():
         st.error("Failed to load data from GitHub.")
         return None
 
-
+st.write("url")
+st.write(data.head())
