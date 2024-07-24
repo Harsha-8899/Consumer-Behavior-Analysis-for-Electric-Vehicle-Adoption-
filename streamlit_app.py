08a71245218b5cc635de3e4dea41d9e5cfc2c248
Harsha-8899 <harshachakravarthy88@gmail.com>
2024-07-23 21:59:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,14 @@ import numpy as np
 st.title("Electric Vehicle Data Analysis")
 st.write("An interactive web app to analyze and visualize electric vehicle data.")
 
-def load_original_data():
-    data = 'https://github.com/Harsha-8899/Consumer-Behavior-Analysis-for-Electric-Vehicle-Adoption-/blob/main/Electric_Vehicle_Population_Data.xlsx'
-    response = requests.get(data)
-    if response.status_code == 200:
-        return pd.read_csv(StringIO(response.text))
-    else:
-        st.error("Failed to load data from GitHub.")
-        return None
+import streamlit as st
+import numpy as np
+import pickle
+import pandas as pd
 
-st.write(data.head())
+https://github.com/Harsha-8899/Consumer-Behavior-Analysis-for-Electric-Vehicle-Adoption-/blob/main/Electric_Vehicle_Population_Data.xlsx.pkl
+joblibFile = open('code_/classifier_model.pkl', 'rb')
+joblibFile = open(url, 'rb')
+model = pickle.load(joblibFile)
+
+model = pd.read_pickle(r'classifier_model.pkl')
